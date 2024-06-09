f2f8a40d532483fbffb9f0cd23e403d51617ec1f
Alex <alex@Aleksandrs-MacBook-Air.local>
2024-06-09 12:33:25 +0100

    test: add burn test

--- test/MyERC20.js
+++ test/MyERC20.js
@@ -21,13 +21,31 @@ describe("MyERC20", function () {
 
     context("Initialization", async function () {
         it("Correctly constructs ERC20 token", async () => {
+            // Check token name
             expect(await token.name()).to.equal("My ERC20 Token");
+
+            // Check token symbol
             expect(await token.symbol()).to.equal("MyERC20");
+
+            // Check token decimals
             expect(await token.decimals()).to.equal(18);
         });
 
         it("Mint tokens", async () => {
+            // Mint initial supply of tokens
             await token.mint(deployer.address, initSupply);
+        });
+
+        it("Burn tokens", async () => {
+            // Try to burn tokens without them being minted
+            await expect(
+                token.burn(100)
+            ).to.be.reverted;
+
+            // Mint tokens first
+            await token.mint(deployer.address, initSupply);
+            // Now it's possible to burn
+            await token.burn(100);
         });
     });
 });
